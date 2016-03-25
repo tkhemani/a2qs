db8e940e93fb8726684852b61d8b7bcaf698f283
tarun khemani <tkhemani@gmail.com>
2016-03-26 01:42:52 +0530

    pipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
 import {DemoBinding} from './demoBinding';
+import {SummaryPipe} from './summary.pipe'
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-    directives: [DemoBinding]
+    directives: [DemoBinding],
+    pipes: [SummaryPipe],
 })
 export class AppComponent { 
-   asdf = "Hello"
+   //asdf = "Hello"
+   largeText = "from the other side....tell you i'm sorry for all the things i've done"
     }
 
