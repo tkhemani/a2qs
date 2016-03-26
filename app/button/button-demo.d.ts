547b81abbd9ad816dab8b49e427f9f2622ee079e
tarun khemani <tkhemani@gmail.com>
2016-03-26 22:48:33 +0530

    cleanup

--- app/button/button-demo.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class ButtonDemo {
-    isDisabled: boolean;
-}
--- app/course.service.ts
+++
@@ -1,6 +0,0 @@
-export class CourseService {
-        private mockCourses = [1,2,3,4];
-getCourses(): number[] {
-    return this.mockCourses;
-}
-}
