8336fcd1a5e89fc35f7109afb73b32f65e41a303
tarun khemani <tkhemani@gmail.com>
2016-03-26 23:38:22 +0530

    all material components added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,11 @@
-import {Component} from 'angular2/core';
-import {ButtonDemo} from './button/button-demo'
-
-
+mport {Component} from 'angular2/core';
 import {MdButton} from '@angular2-material/button';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     // directives: [ButtonDemo]
-    directives: [MdButton, ButtonDemo]
+    ]
 })
 export class AppComponent { 
     }
