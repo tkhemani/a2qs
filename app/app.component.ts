fb81877143e9c7eae8e4269ea3cb3adbdeae56af
tarun khemani <tkhemani@gmail.com>
2016-03-28 03:52:02 +0530

    rx demo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,39 @@
 import {Component, OnInit} from 'angular2/core';
-import {Observable} from 'rxjs/Rx'
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
+import {ControlGroup, FormBuilder} from	'angular2/common';	
 
 @Component({
     selector: 'my-app',
-    template: '<button (click)="clicked($event)">Click Me</button>'
+    template: `
+    <button (click)="clicked($event)">Click Me</button>
+<form	[ngFormModel]="form">
+				<input	type="text"	ngControl="search">
+</form>	
+    `
 })
 export class AppComponent implements OnInit{ 
 
   ngOnInit() {
-    console.clear()
-    let a = 'ngOnInit $loca'
+   
+  }
+  form: ControlGroup;
+
+  constructor(fb: FormBuilder) {
+ 	this.form	=	fb.group({
+										search:	[]
+					});
+					var	search	=	this.form.find('search');
+search.valueChanges
+      .debounceTime(400)
+	  .map(str	=>	console.log("Getting	deals	for	date	"	+	str))	
+	  .subscribe(x	=>	console.log(x))
+						
   }
 
   loca = 10;
   
   clicked($event) {
-    console.log($event);
   }
 }
