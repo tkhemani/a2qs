041722515dbcaadd12c5ba494b37af11b0638e4b
tarun khemani <tkhemani@gmail.com>
2016-03-27 17:55:05 +0530

    cleanup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,15 @@
 import {Component} from 'angular2/core';
-import {MdButton} from '@angular2-material/button';
+// import {MdButton} from '@angular2-material/button';
+// import {MdCard} from '@angular2-material/card';
+// import {MdToolbar} from '@angular2-material/toolbar';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html',
-    directives: [MdButton]
+    template: '<div>WORK NOW</div>'
+//     templateUrl: 'app/app.component.html',
+   // directives: [MdButton, MdCard, MdToolbar]
 })
 export class AppComponent {
-     l = "time is money";
+     l = "HELL";
     }
 
