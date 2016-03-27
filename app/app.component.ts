636a274f108dfae013cc79c4f061707eee714a3a
tarun khemani <tkhemani@gmail.com>
2016-03-27 08:50:53 +0530

    material design

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from 'angular2/core';
 import {MdButton} from '@angular2-material/button';
+import {MdCard} from '@angular2-material/card';
+import {MdToolbar} from '@angular2-material/toolbar';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-    directives: [MdButton]
+    directives: [MdButton, MdCard, MdToolbar]
 })
 export class AppComponent {
-     l = "time is money";
+     l = "HELL";
     }
 
