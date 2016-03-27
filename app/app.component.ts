aa1e1f530e8f0ec79de5ae15ce1c20ca65080558
tarun khemani <tkhemani@gmail.com>
2016-03-27 07:22:13 +0530

    changes to compile ts in browser for faster turnaround time

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,12 @@
-mport {Component} from 'angular2/core';
+import {Component} from 'angular2/core';
 import {MdButton} from '@angular2-material/button';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
-    // directives: [ButtonDemo]
-    ]
+    directives: [MdButton]
 })
-export class AppComponent { 
+export class AppComponent {
+     l = "time is money";
     }
 
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
-
+import {enableProdMode} from 'angular2/core';
+enableProdMode();
 bootstrap(AppComponent);
